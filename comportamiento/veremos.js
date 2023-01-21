031e78fa9bad89c885f485bbe7ddff80ee049009
Raul19AG <ale12012016@gmail.com>
2023-01-21 00:51:04 -0300

    first commit

---
+++ comportamiento/veremos.js
@@ -0,0 +1,50 @@
+/**muestra la informacion de la seccion */
+document.getElementById("info").addEventListener ("click", function (){
+    document.getElementById("infoPer").style.display ="block" ;
+});
+
+/**oculta la info */
+document.getElementById("Inper").addEventListener ("click", function (){
+    document.getElementById("infoPer").style.display ="none" ;
+});
+/**muestra el email de contacto */
+document.getElementById("contacto").addEventListener ("click", function (){
+    document.getElementById("correo").style.display ="block" ;
+});
+/**oculta el amail de contacto */
+document.getElementById("correo").addEventListener ("click", function (){
+    document.getElementById("correo").style.display ="none" ;
+});
+/**muestra sec formacion */
+document.getElementById("forma").addEventListener ("click", function (){
+    document.getElementById("Formacion").style.display ="block" ;
+});
+/**oculta sec formacion */
+document.getElementById("mac").addEventListener ("click", function (){
+    document.getElementById("Formacion").style.display ="none" ;
+});
+/**muestra sec experiencia */
+document.getElementById("expe").addEventListener ("click", function (){
+    document.getElementById("Experiencia").style.display ="block" ;
+});
+/**oculta sec experiencia */
+document.getElementById("Experi").addEventListener ("click", function (){
+    document.getElementById("Experiencia").style.display ="none" ;
+});
+/** muestra la foto */
+document.getElementById("fo").addEventListener ("click", function (){
+    document.getElementById("foto").style.display ="block" ;
+});
+/**oculta la foto */
+document.getElementById("botonF").addEventListener ("click", function (){
+    document.getElementById("foto").style.display ="none" ;
+});
+
+/**muestra el video */
+document.getElementById("vi").addEventListener ("click", function (){
+    document.getElementById("video").style.display ="block" ;
+});
+/**oculta video */
+document.getElementById("vid").addEventListener ("click", function (){
+    document.getElementById("video").style.display ="none" ;
+});
